b64a9c2c2529a49e725ebe910d1e0617d09f3b1b
marcelomatz <omarcelomatz@gmail.com>
2024-09-12 16:55:46 -0300

    ajuste no bg header

--- src/components/Main/Hero/Hero.tsx
+++ src/components/Main/Hero/Hero.tsx
@@ -2,7 +2,7 @@ import ContentHero from "./ContentHero";
 
 export default function Hero() {
   return (
-    <div className="w-full bg-no-repeat bg-uber bg-cover bg-right duration-300 mt-14 sm:mt-20 sm:p-10">
+    <div className="w-full bg-no-repeat sm:bg-uber bg-cover bg-right duration-300 mt-14 sm:mt-20 sm:p-10">
       <ContentHero />
     </div>
   );
